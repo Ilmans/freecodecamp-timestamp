2a178aaf155bde3732731b6fe4c9c916d08cac1b
Ilman Sunanuddin <ilmansunannudin2@gmail.com>
2024-08-23 16:26:12 +0700

    whoami

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const apiRouter = express.Router();
 
+//task 1
 apiRouter.get("/:date?", (req, res) => {
   const date = req.params.date;
   let dateObj = !date
@@ -16,7 +17,14 @@ apiRouter.get("/:date?", (req, res) => {
 
   res.json({ unix: dateObj.getTime(), utc: dateObj.toUTCString() });
 });
+// task 2
+apiRouter.get("/whoami", (req, res) => {
+  const ipaddress = req.ip;
+  const language = req.headers["accept-language"];
+  const software = req.headers["user-agent"];
+  res.json({ ipaddress, language, software });
+});
 
-apiRouter.get("/ti")
+apiRouter.get("/ti");
 
 module.exports = apiRouter;
